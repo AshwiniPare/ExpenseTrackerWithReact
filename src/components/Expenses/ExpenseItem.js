45d34449d7d5e72a12c42c27cc200df38fd19d1a
AshwiniPare <ashwinipbhatt@gmail.com>
2023-07-26 16:21:53 -0700

    adding event listener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,11 +5,24 @@ import ExpenseDetail from './ExpenseDetail';
 import Card from '../UI/Card'
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("Clicked!!!");
+  };
+
+  const deleteHandler = () => {
+    console.log("Clicked!!! + ");
+    var gfg_down =
+                document.getElementById(props.id);
+               // gfg_down.parentElement.removeChild(gfg_down);
+               // gfg_down.parentNode.removeChild(gfg_down);
+  };
+
  return (
     <Card className="expense-item">
      <ExpenseDate date={props.date}></ExpenseDate>
       <ExpenseDetail amount={props.amount} location={props.location} title={props.title}></ExpenseDetail>
-      
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deleteHandler}>Delete Expense</button>
     </Card>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,20 +6,16 @@ import './Expenses.css'
 function Expenses(props) {
     return (
         <Card className="expenses">
-        <ExpenseItem 
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-          location={props.expenses[0].location}
-         >
+          
+
+      {props.expenses.map(item => (
+        <ExpenseItem  key={item.id}
+        title={item.title}
+        amount={item.amount}
+        date={item.date}
+        location={item.location}>
         </ExpenseItem>
-        <ExpenseItem 
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-          location={props.expenses[1].location}
-         >
-        </ExpenseItem>
+      ))}
       </Card>
     );
 }
