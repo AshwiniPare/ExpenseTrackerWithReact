bde69f3c58adff606a8ed3c619a83c98abdb4a81
AshwiniPare <ashwinipbhatt@gmail.com>
2023-07-30 15:45:09 +0530

    expense form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Expenses from './components/Expenses/Expenses'
+import  NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   /*const expenses = [
@@ -29,18 +30,19 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
- return React.createElement(
+ /*return React.createElement(
     'div', 
     {}, 
     React.createElement('h2', {}, "getting started"),
     React.createElement(Expenses, {expenses: expenses})
-  );
-  /*return (
+  );*/
+  return (
       <div >
+        <NewExpense />
       <Expenses expenses={expenses}></Expenses>
         
     </div>
-  );*/
+  );
 }
 
 export default App;
